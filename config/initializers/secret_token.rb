11c22c8c4fd275be16945af8f6e3cf5a686ff83d
Yvonne <ciaoyvonne@gmail.com>
2012-10-31 14:35:56 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeBananas::Application.config.secret_token = 'e5172739a55d871e56aa2b14d697dd13d1aa485867db3692ea6f6608f72f3f70d850f45c3acf3181677c444337d8dc2707f11b0e66da6e6116cd11c359017861'
