23fa223572652763eba66712cfdede9e319dccde
Chris <ciaoyvonne@gmail.com>
2012-10-31 21:46:26 -0700

    Reenabled route to static/home

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ WeBananas::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
 
-  #get "static_pages/home"
+  get "static_pages/home"
 
   #get "static_pages/about"
   #get "static_pages/help"
